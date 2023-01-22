8ae78416031774798daa1cde6b55275688bf8010
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-01-21 20:08:27 -0800

    add HeaderCart button and style it

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
 import MealHeaderImage from "./../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={MealHeaderImage} alt="A full meal on a dining table"></img>
