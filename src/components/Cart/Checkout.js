f30b76c3f0d59da9b8718b193190e9b3956543c3
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-02-12 21:31:25 -0800

    access the entered values by user in the form while cordering

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,28 +1,37 @@
 import classes from "./Checkout.module.css";
+import { useRef } from "react";
 
 const Checkout = (props) => {
+  const nameRef = useRef();
+  const streetRef = useRef();
+  const cityRef = useRef();
+  const zipRef = useRef();
   const onConfirmHandler = (event) => {
     event.preventDefault();
-    console.log("submitting");
+    const enteredName = nameRef.current.value;
+    const enteredStreet = streetRef.current.value;
+    const enteredCity = cityRef.current.value;
+    const enteredZip = zipRef.current.value;
+    console.log(enteredName, enteredStreet, enteredCity, enteredZip);
   };
 
   return (
     <form className={classes.form} onSubmit={onConfirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your name</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" ref={nameRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street Address</label>
-        <input id="street" type="text" />
+        <input id="street" type="text" ref={streetRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input id="city" type="text" />
+        <input id="city" type="text" ref={cityRef} />
       </div>
       <div className={classes.control}>
         <label htmlFor="zip">Zip Code</label>
-        <input id="zip" type="text" />
+        <input id="zip" type="text" ref={zipRef} />
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
