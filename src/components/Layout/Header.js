30781aeed4beabf0d2936e48928a4f9693b7d761
Asha Luckins <ashaluckinsanoop@gmail.com>
2023-04-29 20:05:38 -0400

    demo

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
-        <h1>React Meals</h1>
+        <h1>React Meals DEmo</h1>
         <HeaderCartButton onCartClick={props.onCartClick} />
       </header>
       <div className={classes["main-image"]}>
